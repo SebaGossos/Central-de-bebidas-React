ecbed58e2617912e6307c6c6005a90f1c483a091
Sebastián Nicolás Gossos Scribe <111235284+WinniGossos33@users.noreply.github.com>
2023-04-05 14:19:20 -0300

    actualizando entrega

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import { useState } from 'react'
-import { NavBar } from './components/navBar/navBar'
-import './App.css'
+import { NavBar } from './components/NavBar/NavBar'
+import '../build/css/app.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <NavBar />
-    </div>
+    <NavBar/>
   )
 }
 
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,23 +1,34 @@
+import '../../../build/css/app.css'
 export function NavBar(){
     const logoCentralBebidas = "./src/assets/img/central.png"
 
     return(
-        <section>
+        <header className='contenedor header'>
             <Logo ruta={logoCentralBebidas}/>
             <Opciones name1='Iniciar Sesion' name2='Registrarse'/>  
-        </section>
+        </header>
     )
 }
 
 
-function Logo(props){
-    return <img src={props.ruta} alt="Logo" />
+function Logo(){
+    return (
+        <div className="header__logo">
+            <a href="index.html">
+                <picture>
+                    <source srcset="build/img/central.avif" type="image/avif"/>
+                    <source srcset="build/img/central.webp" type="image/webp"/>
+                    <img className="header__imagen" loading="lazy" src="build/img/central.png" width="" height="" alt="logo Central Bebidas" />
+                </picture>
+            </a>
+        </div>
+    )
 }
-
-
+        
 function Opciones(props){
-    return  <nav>
-                <button>{props.name1}</button>
-                <button>{props.name2}</button>
+    return  <nav class="registro">
+                <button className="registro__inicioSesion">{props.name1}</button>
+                <button className="registro__crearCuenta">{props.name2}</button>
+                <div className='registro__carrito'>3🛒</div>
             </nav>
 }
